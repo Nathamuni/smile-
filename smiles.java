6bd5d4e7dfe2b354cbc301f6f5540ac3ad349f8e
Nathamuni <87070459+Nathamuni@users.noreply.github.com>
2022-09-01 21:04:24 +0530

    The verrry last commit

--- smiles.java
+++ smiles.java
@@ -1,23 +1,36 @@
 import java.util.ArrayList;
 
- class smiles1 {
+public class smiles {
+    /*    public static int hashmap(String s)
+        {
+            HashMap<String,Integer> val = new HashMap<>();
+            val.put("c",4);
+            val.put("C",4);
+            val.put("N",3);
+            val.put("O",2);
+          //  val.put("Cl",1);
+            val.put("H",1);
+            val.put("-",1);
+            val.put("=",2);
+            val.put("#",3);
 
+            return val.get(s);
+        }*/
     public static void arr(String s){
 
         int count=0;
         String s1 = s.replaceAll("[-=#]","");
-
+        System.out.println("The adj matrix4 : "+s1);
         char[] str1 = s1.toCharArray();
 
         ArrayList<Integer> array1 = new ArrayList<Integer>();
         for(int i=0; i< str1.length; i++){
             if (str1[i] =='('){
-                //  array1[count]=i - count*2;
+              //  array1[count]=i - count*2;
                 array1.add(i - count*2);
                 count++;
             }
         }
-
         ArrayList<Character> ar = new ArrayList<>();
 
         for(int j=0;j<str1.length;j++)
@@ -47,35 +60,32 @@ import java.util.ArrayList;
                 count++;
             }
             else {
-                if(array.contains(i-1)){
-                    arr[i][i-2]=1;
-                    arr[i][i+1]= 1;
-                }
-                else
-                    arr[i][i - 1] = arr[i][i + 1] = 1;
-
+                arr[i][i - 1] = arr[i][i + 1] = 1;
             }
         }
-        if(array.size() !=0 && i == size-1 && i-1==array.get(count-1)){
-            arr[i][i-2]=1;
-        }else{
-            arr[i][i-1]=1;
+
+        if(array.size() !=0 && i == size-1 && i-1==array.get(count-1)) {
+            arr[i][i - 2] = 1;
+
+        }else {
+            arr[i][i - 1] = 1;
         }
         print(size,arr,ar);
     }
 
 
     public static void print(int size,int[][] arr,ArrayList<Character> ar){
+        System.out.println("  "+ar);
         for(int i=0; i< size ;i++){
-           System.out.print(ar.get(i)+"| ");
+            System.out.print(ar.get(i)+"| ");
             for(int j=0; j<size; j++){
-                System.out.print(arr[i][j]+" ");
+                System.out.print(arr[i][j]+"  ");
             } System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        arr("C=(c)-c-c");
+        arr("C=(c)-c-(o)-c");
 
     }
 }
