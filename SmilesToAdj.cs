57a300b7b14c9ab6b981c761c1d4b0512debc13b
Nathamuni <87070459+Nathamuni@users.noreply.github.com>
2023-01-16 20:55:03 +0530

    almost final commit😎

--- SmilesToAdj.cs
+++ SmilesToAdj.cs
@@ -9,7 +9,7 @@ namespace xyz
     class WeightedGraph
     {
         //INPUT SMILES
-        static string sb = "C-C(-C(=O)-O)-C"; 
+        static string sb = "C-Cl(-C)-C-C"; 
         //CONVERTING THE INPUT SMILES STRING INTO CHARACTER ARRAY-->RES
         //FOR EASY TRAVERSAL
         static char[] res = sb.ToCharArray();
@@ -31,6 +31,11 @@ namespace xyz
             {'-',1},{'=',2},{'#',3},{'$', 4},{'.', 0}
             // {':',1.5},
         };
+
+        // public static readonly Dictionary<char,int> organicSubset = new Dictionary<String, int>()
+        // {//B, C, N, O, P, S, F, Cl, Br, and I
+        //     //{"B",3},{"C",4},{"O",2},{"P",5},{"P",3},{'S',6},{'S',4},{'S',2},{'F',1},{'Cl',1},{'Br',1},{'I',}
+        // }
 
         //CONSTRUCTOR 
         public WeightedGraph()
@@ -92,13 +97,17 @@ namespace xyz
             {
                 Console.WriteLine(ele.Key +"-->"+ ele.Value);
             }
-            //Console.WriteLine(graph.adjacencyList.Count);
         }
-        static void adjacencyMatrix()
+        static void adjacencyMatrix(int size)
         {
             //int Matrix_size = graph.adjacencyList.Count;
             int Matrix_size = res.Length; 
             int[,] arr = new int[Matrix_size,Matrix_size];
+            int sum1=0;
+            int[,] nums = new int[size,size];
+            int[] arr1 = new int[Matrix_size];
+            int n=0,m=0;
+
             foreach(var x in graph.adjacencyList)
             {
                 foreach(KeyValuePair<int, int> y in x.Value)
@@ -106,14 +115,52 @@ namespace xyz
                     arr[x.Key,y.Key] = 1;
                 }
             }
+
             for(int i=0;i<Matrix_size;i++)
             {
-                Console.WriteLine();
+                sum1=0;
                 for(int j=0;j<Matrix_size;j++)
                 {
-                    Console.Write(arr[i,j]+" ");
+                    sum1=sum1+arr[i,j];
+                }
+                if(sum1!=0) arr1[i]=1;
+            }
+            for(int i=0;i<arr1.Length;i++) Console.Write(arr1[i]+" ");
+            Console.WriteLine();
+            for(int i=0;i<Matrix_size;i++)
+            {
+                if(arr1[i]!=0)
+                {
+                    n=0;
+                    for(int j=0;j<Matrix_size;j++)
+                    {
+                        if(arr1[j]!=0) 
+                        {
+                            nums[m,n]=arr[i,j];
+                            n++;
+                        }
+                    }
+                    m++;
                 }
             }
+            Console.WriteLine();
+            for(int i=0;i<size;i++)
+            {
+                for(int j=0;j<size;j++)
+                {
+                    Console.Write(nums[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+
+            // for(int i=0;i<Matrix_size;i++)
+            // {
+            //     Console.WriteLine();
+            //     for(int j=0;j<Matrix_size;j++)
+            //     {
+            //         Console.Write(arr[i,j]+" ");
+            //     }
+            // }
         }
         //MAIN METHOD
         static void Main(string[] args)
@@ -124,16 +171,31 @@ namespace xyz
 
             for (int i = 0; i < res.Length; i++)
             {
-                if( bonds.ContainsKey(res[i]) || (res[i] == ('(')) || (res[i] == (')')) || Char.IsDigit(res[i]))
+                if( bonds.ContainsKey(res[i]) || (res[i] == ('(')) || (res[i] == (')')) || Char.IsDigit(res[i]) || (char.IsLower(res[i])))
                 {
                     continue;
                 }
                 else 
                 {
-                    res2.Add(res[i]);
-                    graph.addVertex(i);
+                    // c-c
+                    // res2.Add(res[i]);
+                    // graph.addVertex(i);
+                    // if(char.IsLower(res[i]))
+                    // {
+                    //     res2.Add(res[i-1]);
+                    //     Console.Write(i-1+" ");
+                    //     graph.addVertex(i-1);
+                    // }
+                    //else
+                    //{
+                    //}
+                        res2.Add(res[i]);
+                        Console.Write(i+" ");
+                        graph.addVertex(i);
                 }
-            }            
+            }    
+
+            Console.WriteLine();        
 
             //PRINT THE LIST RES-->CONTAINS INPUT SMILES IN CHARACTER ARRAY FORM
             for(int i=0;i<res.Length;i++)
@@ -158,27 +220,43 @@ namespace xyz
                 //CHECK/LOGIC FOR RING STRUCTURES IN THE INPUT SMILES
                 if(char.IsDigit(res[i]))
                 {
+                    //int idx1=i,idx2=i+1;
                     for(int j=i+1;j<res.Count();j++)
                     {
-                        if(res[i]==res[j]) graph.addEdge(i-1,j-1,1);
+                        //idx2=j;
+                        if(res[i]==res[j])//break;
+                        {
+                            if(char.IsLower(res[i-1])&&(char.IsLower(res[j-1]))) graph.addEdge(i-2,j-2,1);
+                            else if(char.IsLower(res[i-1])) graph.addEdge(i-2,j-1,1);
+                            else if(char.IsLower(res[j-1])) graph.addEdge(i-1,j-2,1);
+                            else graph.addEdge(i-1,j-1,1);        
+                        }
+                        //graph.addEdge(i-1,j-1,1);
                     }
                 }
-                //CHECK/LOGIC FOR BRANCH STRUCTURES IN INPUT SMILES
+                //CHECK/LOGIC FOR BRANCH STRUCTURES IN INPUT SMILES NOTATION
                 if((res[i]==')'))
                 {
                     count--;
                     if(i!=res.Length-1)
                     {
-                        if((res[i+1]=='-')||(res[i+1]=='=' )||(res[i+1]=='#'))
+                        if(bonds.ContainsKey(res[i+1]))
                         {
-                        int bond = graph.findindexvalues(i,0);
-                        graph.addEdge(i+2,arr[count],bond);
+                            int bond = graph.findindexvalues(i,0);
+                            graph.addEdge(i+2,arr[count],bond);
                         }
                     }
                 }
                 if((res[i]=='(')&&(res[i-1]!=')'))
                 {
-                    arr[count] = i-1;
+                    if(char.IsLower(res[i-1]))
+                    {
+                        arr[count] = i-2;
+                    }
+                    else 
+                    {
+                        arr[count] = i-1;
+                    }
                     count++;
                 } 
                 if((res[i]=='(')&&(res[i-1]==')'))
@@ -189,37 +267,63 @@ namespace xyz
                 else if((res[i]=='(')&&(res[i-1]!=')'))
                 {
                     int bond = graph.findindexvalues(i,0);
-                    graph.addEdge(i-1,i+2,bond);
+                    // graph.addEdge(i-1,i+2,bond);
+                    if(char.IsLower(res[i-1]))
+                    {
+                        graph.addEdge(i-2,i+2,bond);
+                    }
+                    else
+                    {
+                        graph.addEdge(i-1,i+2,bond);
+                    }
                 }
-                //CHECK/LOGIC FOR BONDS IN INPUT SMILES
+                //CHECK/LOGIC FOR BONDS IN INPUT SMILES NOTATION
                 if((bonds.ContainsKey(res[i]))&&(res[i-1]!=('('))&&(res[i-1]!=(')')))
                 {
                     if(char.IsDigit(res[i-1]))
                     {
-                        graph.addEdge(i-2,i+1,bonds[res[i]]);
+                        if(char.IsLower(res[i-2]))
+                        {
+                            graph.addEdge(i-3,i+1,bonds[res[i]]);
+                        }
+                        else
+                        {
+                            graph.addEdge(i-2,i+1,bonds[res[i]]);
+                        }
+                        //graph.addEdge(i-2,i+1,bonds[res[i]]);
                     }
                     else
                     {
-                        graph.addEdge(i-1,i+1,bonds[res[i]]);
+                        //graph.addEdge(i-1,i+1,bonds[res[i]]);
+                        if(char.IsLower(res[i-1]))
+                        {
+                            graph.addEdge(i-2,i+1,bonds[res[i]]);
+                        }
+                        else
+                        {
+                            graph.addEdge(i-1,i+1,bonds[res[i]]);
+                        }
                     }
                 }
             }
 
             //IMPLICIT HYDROGEN INITIALIZATION
-            implicitHydrogenIndex();
+            //implicitHydrogenIndex();
             //ADJACENCY MATRIX GENERATION
-            adjacencyMatrix();
+            adjacencyMatrix(res2.Count);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
             //PRINTING THE GRAPH
-            foreach (var x in graph.adjacencyList)
-            {
-                foreach (KeyValuePair<int, int> y in x.Value)
-                {
-                    Console.WriteLine("vertex " + x.Key + " ---> " + y.Key + ": "+y.Value);
-                }
-            }
+            // foreach (var x in graph.adjacencyList)
+            // { 
+            //     Console.WriteLine(x.Key);
+            //     foreach (KeyValuePair<int, int> y in x.Value)
+            //     {
+            //         Console.WriteLine("vertex " + x.Key + " ---> " + y.Key + ": "+y.Value);
+            //     }
+            // }
         }
     }
 }
+
